b3f53e5b022819f4c19e4b6ce00d53306316f026
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2026-03-14 20:11:56 -0300

    api teste

--- Quasar BET/projeto/quasarbet-api/src/main/java/com/quasarbet/api/controller/TesteApiController.java
+++ Quasar BET/projeto/quasarbet-api/src/main/java/com/quasarbet/api/controller/TesteApiController.java
@@ -1,14 +1,25 @@
 package com.quasarbet.api.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesteApiController {
 
-    @GetMapping("/")
-    public String home() {
-        return "QuasarBET API : OK";
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/teste")
+    public Map<String, Object> home() {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", true);
+        response.put("message", "QuasarBET API TESTE: OK");
+
+        return response;
     }
 
 }
