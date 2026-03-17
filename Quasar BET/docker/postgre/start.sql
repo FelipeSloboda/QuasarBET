875e2079c34cb506ea97eb3f202abe0f8e0df27c
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2026-03-16 21:40:09 -0300

    criado tabela de usuarios

--- Quasar BET/docker/postgre/start.sql
+++ Quasar BET/docker/postgre/start.sql
@@ -11,22 +11,118 @@ INSERT INTO instalacao_tb (descricao, data)
 VALUES ('INSTALACAO OK', NOW());
 
 
-/* TESTE_TB */
-CREATE TABLE IF NOT EXISTS teste_tb (
-    id        SERIAL PRIMARY KEY,
-    descricao VARCHAR(255)
+/* USUARIOS */
+CREATE TABLE IF NOT EXISTS usuarios_tb (
+    id                   SERIAL PRIMARY KEY,
+    cpf                  CHAR(11) UNIQUE NOT NULL,
+    email                VARCHAR(254) UNIQUE NOT NULL,
+    senha                TEXT NOT NULL,
+    nome                 VARCHAR(100) NOT NULL,
+    sobrenomes           VARCHAR(150) NOT NULL,
+    data_nascimento      DATE NOT NULL,
+    status               VARCHAR(20) NOT NULL DEFAULT 'pendente',
+    email_verificado     BOOLEAN NOT NULL DEFAULT FALSE,
+    telefone_verificado  BOOLEAN NOT NULL DEFAULT FALSE,
+    ultimo_login         TIMESTAMPTZ,
+    tentativas_login     INT NOT NULL DEFAULT 0,
+    bloqueio_login_ate   TIMESTAMPTZ,
+    data_cadastro        TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_atualizacao     TIMESTAMPTZ
 );
 
-INSERT INTO teste_tb (descricao)
-VALUES ('TESTE 1');
-
-
-/* USER_TB */
-CREATE TABLE IF NOT EXISTS usuario_tb (
-    id      SERIAL PRIMARY KEY,
-    usuario VARCHAR(255) UNIQUE NOT NULL,
-    senha   VARCHAR(255) NOT NULL
+/* TELEFONES */
+CREATE TABLE IF NOT EXISTS telefones_tb (
+    id                   SERIAL PRIMARY KEY,
+    usuario_id           INT NOT NULL,
+    codigo_pais          SMALLINT NOT NULL,
+    ddd                  SMALLINT NOT NULL,
+    telefone             VARCHAR(15) UNIQUE NOT NULL,
+    data_cadastro        TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,      
+    data_atualizacao     TIMESTAMPTZ,
+    CONSTRAINT fk_telefones_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE
 );
 
-INSERT INTO usuario_tb (usuario, senha)
-VALUES ('admin', '$2a$12$4dRfsC/exd4UT2R9xh4T0uBoYK2EyBEJrwwsH.exX/wbcqX8951yK');
+/* ENDERECOS */
+CREATE TABLE IF NOT EXISTS enderecos_tb (
+    id                   SERIAL PRIMARY KEY,
+    usuario_id           INT NOT NULL,
+    cep                  VARCHAR(8) NULL,
+    endereco             VARCHAR(150) NULL,
+    complemento          VARCHAR(50) NULL,
+    num                  VARCHAR(10) NULL,
+    bairro               VARCHAR(50) NULL,
+    cidade               VARCHAR(50) NULL,
+    estado               VARCHAR(50) NULL,
+    pais                 VARCHAR(50) NULL,
+    data_cadastro        TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_atualizacao     TIMESTAMPTZ,
+    CONSTRAINT fk_enderecos_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE
+);
+
+/* CARTEIRAS */
+CREATE TABLE IF NOT EXISTS carteiras_tb (
+    id                   SERIAL PRIMARY KEY,
+    usuario_id           INT NOT NULL,
+    saldo                NUMERIC(15,2) NOT NULL DEFAULT 0.00,
+    data_cadastro        TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_atualizacao     TIMESTAMPTZ,
+    CONSTRAINT fk_carteiras_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE
+);
+
+/* INDICACOES */
+CREATE TABLE IF NOT EXISTS indicacoes_tb (
+    id                   SERIAL PRIMARY KEY,
+    usuario_id           INT NOT NULL,
+    codigo_indicacao     VARCHAR(50) NOT NULL,
+    usuario_id_indicador INT NULL,
+    data_cadastro        TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_atualizacao     TIMESTAMPTZ,
+    CONSTRAINT fk_indicacoes_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE,
+    
+    CONSTRAINT fk_indicacoes_usuario_indicador
+        FOREIGN KEY (usuario_id_indicador)
+        REFERENCES usuarios_tb(id)
+        ON DELETE SET NULL
+);
+
+/* SESSOES */
+CREATE TABLE IF NOT EXISTS sessoes_tb (
+    id                  SERIAL PRIMARY KEY,
+    usuario_id          INT NOT NULL,
+    token               VARCHAR(254) NOT NULL,
+    ip                  VARCHAR(39) NOT NULL,   
+    dados_requisicao    TEXT NOT NULL,
+    data_cadastro       TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    data_expiracao      TIMESTAMPTZ ,
+    expirado            BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT fk_sessoes_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE
+);
+
+/* HISTORICOS_LOGIN */
+CREATE TABLE IF NOT EXISTS historicos_login_tb (
+    id                  SERIAL PRIMARY KEY,
+    usuario_id          INT NOT NULL,
+    ip                  VARCHAR(39) NOT NULL,   
+    dados_requisicao    TEXT NOT NULL,
+    status              VARCHAR(10) NOT NULL,
+    data                TIMESTAMPTZ,
+    CONSTRAINT fk_historicos_login_usuario
+        FOREIGN KEY (usuario_id)
+        REFERENCES usuarios_tb(id)
+        ON DELETE CASCADE
+);
