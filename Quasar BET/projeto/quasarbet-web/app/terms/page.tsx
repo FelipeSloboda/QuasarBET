cc21768c55d8b8ec18d0028260f5642e6e3c3507
FelipeSloboda <felipe.ti.sloboda@gmail.com>
2026-05-03 16:31:32 -0300

    FRONTEND: PAGINA TERMOS (INICIO)

---
+++ Quasar BET/projeto/quasarbet-web/app/terms/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Terms() {
+  return (
+    <div>
+      <p>Terms and Conditions</p>
+
+        <br/>
+
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/login">Login</Link></li>
+        <li><Link href="/forgot-password">Forgot Password</Link></li>
+        <li><Link href="/reset-password">Reset Password</Link></li>
+        <li><Link href="/register">Register</Link></li>
+        <li><Link href="/confirm-email-change">Confirm Email Change</Link></li>
+        <li><Link href="/dashboard">Dashboard</Link></li>
+      </ul>
+    </div>
+  );
+}
